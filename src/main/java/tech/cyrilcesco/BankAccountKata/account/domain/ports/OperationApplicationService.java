485299b83983a596427c376d2c632de62529f930
cyril <cyril.cesco@gmail.com>
2021-07-26 22:47:54 +0200

    [US 1]: Implementation de OperationApplicationService qui permet de faire un dépot

--- src/main/java/tech/cyrilcesco/BankAccountKata/account/domain/ports/OperationApplicationService.java
+++ src/main/java/tech/cyrilcesco/BankAccountKata/account/domain/ports/OperationApplicationService.java
@@ -3,6 +3,10 @@ package tech.cyrilcesco.BankAccountKata.account.domain.ports;
 import org.springframework.stereotype.Service;
 import tech.cyrilcesco.BankAccountKata.account.domain.model.Operation;
 
+import java.math.BigDecimal;
+import java.util.Comparator;
+import java.util.Objects;
+
 @Service
 public class OperationApplicationService {
 
@@ -13,6 +17,16 @@ public class OperationApplicationService {
     }
 
     public Operation deposit(Operation operation) {
-        return null;
+        Operation lastOperationToGetAccountBalance =
+                operationRepository.getAllOperations(operation.getAccount().getId()).stream()
+                        .filter(Objects::nonNull)
+                        .max(Comparator.comparing(Operation::getDate))
+                        .orElseGet(() -> Operation.builder().accountBalanceAfterOperation(BigDecimal.ZERO).build());
+        return operationRepository.save(getOperationWithLastAccountBalanceAmountAdded(lastOperationToGetAccountBalance, operation));
+    }
+
+    private Operation getOperationWithLastAccountBalanceAmountAdded(Operation lastOperation, Operation operationToAdd) {
+        BigDecimal accountBalanceAfterOperation = operationToAdd.getAmount().add(lastOperation.getAccountBalanceAfterOperation());
+        return operationToAdd.toBuilder().accountBalanceAfterOperation(accountBalanceAfterOperation).build();
     }
 }
