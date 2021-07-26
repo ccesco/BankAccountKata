36fe587fee8dbefec88f642f0fedad063178f900
cyril <cyril.cesco@gmail.com>
2021-07-26 19:07:59 +0200

    initialisation du projet maven

---
+++ src/main/java/tech/cyrilcesco/BankAccountKata/BankAccountKataApplication.java
@@ -0,0 +1,13 @@
+package tech.cyrilcesco.BankAccountKata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/cyrilcesco/BankAccountKata/BankAccountKataApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.cyrilcesco.BankAccountKata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
