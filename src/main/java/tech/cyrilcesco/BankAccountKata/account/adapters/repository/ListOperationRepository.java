3cb3ee928a4fa6d12d586404c02f75668380f856
cyril <cyril.cesco@gmail.com>
2021-07-26 21:37:34 +0200

    [US 1]: implementation du repository

--- src/main/java/tech/cyrilcesco/BankAccountKata/account/adapters/repository/ListOperationRepository.java
+++ src/main/java/tech/cyrilcesco/BankAccountKata/account/adapters/repository/ListOperationRepository.java
@@ -1,22 +1,28 @@
 package tech.cyrilcesco.BankAccountKata.account.adapters.repository;
 
+import org.springframework.stereotype.Service;
 import tech.cyrilcesco.BankAccountKata.account.domain.model.Operation;
 import tech.cyrilcesco.BankAccountKata.account.domain.model.OperationType;
 import tech.cyrilcesco.BankAccountKata.account.domain.ports.OperationRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class ListOperationRepository implements OperationRepository {
+
+    private List<Operation> operations = new ArrayList<>();
 
     @Override
     public Operation save(Operation operation) {
-        return null;
+        operations.add(operation);
+        return operation;
     }
 
     @Override
     public Optional<Operation> getOperation(int operationId) {
-        return null;
+        return operations.stream().filter((Operation operation) -> operation.getId() == operationId).findFirst();
     }
 
     @Override
